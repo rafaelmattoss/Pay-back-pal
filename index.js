5e81207718754b7163370e157c2419559cc774d7
Rafael Matos <rafaelmattos001@gmail.com>
2024-05-04 18:54:15 -0300

    new

--- index.js
+++ index.js
@@ -1,161 +1,90 @@
- 
-    
-    $("#add").click(function() {
-        $("#form-adiciona").toggle();
-    });
-    
-    let clienteEditando = null; // Variável para armazenar o cliente sendo editado
 
-    // Evento para mostrar o formulário de edição ao clicar no botão "Editar Cliente"
-    $("body").on("click", ".editar", function() {
-      
-        clienteEditando = $(this).closest(".cliente");
-        
-        // Preenche o formulário de edição com os dados do cliente
-        $("#novonome").val(clienteEditando.find(".nome").text());
-        $("#novotelefone").val(clienteEditando.find(".telefone").text());
-        $("#novoendereco").val(clienteEditando.find(".endereco").text());
-        $("#novovalor").val(clienteEditando.find(".valor").text());
-        $("#novotaxa").val(clienteEditando.find(".juros").text());
-        $("#novoqtdparcelas").val(clienteEditando.find(".numeroparcelas").text());
-
-        // Mostra o formulário de edição
-        $("#alteracao").show();
+    $(document).ready(function() {
+        // Evento para mostrar/ocultar o formulário de adição ao clicar no botão "Adicionar Cliente"
+        $("#add").click(function() {
+            $("#form-adiciona").toggle();
+        });
     
-    });
-
-   
-    $("#salvar").click(function(event) {
-        event.preventDefault();
+        // Evento para adicionar um novo cliente ao clicar no botão "Adicionar cliente"
+        $("#adcliente").click(function(event) {
+            event.preventDefault();
     
-        const novoNome = $("#novonome").val();
-        const novoTelefone = $("#novotelefone").val();
-        const novoEndereco = $("#novoendereco").val();
-        const novoValor = parseFloat($("#novovalor").val());
-        const novoTaxa = parseFloat($("#novotaxa").val());
-        const novoParcelas = parseInt($("#novoqtdparcelas").val());
+            if (validarCliente()) {
+                adicionarCliente();
+                limparCampos();
+            }
+        });
     
-        const taxaDecimal = novoTaxa / 100;
-        const novoTotal = novoValor * (1 + taxaDecimal);
-        const novoValorParcela = novoTotal / novoParcelas;
+        // Função para validar os campos do formulário de adição
+        function validarCliente() {
+            const nome = $("#adnome").val();
+            const telefone = $("#adtelefone").val();
+            const endereco = $("#adendereco").val();
+            const valor = $("#advalor").val();
+            const taxa = $("#adtaxa").val();
+            const parcelas = $("#adparcelas").val();
     
-        // Atualizar os elementos HTML do cliente sendo editado
-        clienteEditando.find(".nome").text(novoNome);
-        clienteEditando.find(".telefone").text(novoTelefone);
-        clienteEditando.find(".endereco").text(novoEndereco);
-        clienteEditando.find(".valor").text(novoValor);
-        clienteEditando.find(".valorparcela").text(novoValorParcela.toFixed(2));
-        clienteEditando.find(".numeroparcelas").text(novoParcelas);
-        clienteEditando.find(".total").text(novoTotal.toFixed(2));
-        clienteEditando.find(".juros").text(novoTaxa + "%");
+            if (nome === '' || telefone === '' || endereco === '' || valor === '' || taxa === '' || parcelas === '') {
+                alert("Por favor, preencha todos os campos.");
+                return false;
+            }
     
-        // Ocultar o formulário de edição após salvar
-        $("#alteracao").hide();
-    });
-    
-    
-  
-    $("#adcliente").click(function(event) {
-        event.preventDefault();
-
-        if (validarCliente()) {
-            adicionarCliente();
-            limparCampos();
+            return true;
         }
-    });
+    
+        // Função para adicionar um novo cliente à lista
+        function adicionarCliente() {
+            const cliente = {
+                nome: $("#adnome").val(),
+                telefone: $("#adtelefone").val(),
+                endereco: $("#adendereco").val(),
+                comercio: $("#comercio").val(),
+                valor: parseFloat($("#advalor").val()),
+                juros: parseFloat($("#adtaxa").val()),
+                parcelas: parseInt($("#adparcelas").val())
+            };
+    
+            const taxaDecimal = cliente.juros / 100;
+            const valorTotal = cliente.valor * (1 + taxaDecimal);
+            const valorParcela = valorTotal / cliente.parcelas;
+    
+            // Criar novo elemento div para representar o cliente
+            const novoCliente = $("<div>").addClass("cliente");
+    
+            $("<h2>").text(cliente.nome).addClass("nome").appendTo(novoCliente);
+            $("<p>").text("Endereço: " + cliente.endereco).addClass("endereco").appendTo(novoCliente);
+            $("<p>").text("Tipo de Comércio: " + cliente.comercio).addClass("tipocomercio").appendTo(novoCliente);
+            $("<p>").text("Telefone: " + cliente.telefone).addClass("telefone").appendTo(novoCliente);
+            $("<p>").text("Valor Emprestado: " + cliente.valor).addClass("valor").appendTo(novoCliente);
+            $("<p>").text("Valor da Parcela: " + valorParcela.toFixed(2)).addClass("valorparcela").appendTo(novoCliente);
+            $("<p>").text("Número de Parcelas: " + cliente.parcelas).addClass("numeroparcelas").appendTo(novoCliente);
+            $("<p>").text("Taxa de Juros: " + cliente.juros + "%").addClass("juros").appendTo(novoCliente);
+            $("<p>").text("Total a Pagar: " + valorTotal.toFixed(2)).addClass("total").appendTo(novoCliente);
+            $("<br>").appendTo(novoCliente);
+    
+           
+            const divRetirada = $("<div>").attr("id", "retirada").appendTo(novoCliente);
+            $("<p>").attr("id", "textosaida").text("Digite o numero de parcelas pagas por  "+ cliente.nome).appendTo(divRetirada);
+            $("<input>").attr("type", "text").attr("name", "pagamentos").attr("id", "pagamentos").appendTo(divRetirada);
+            $("<button>").text("Realizar Saída").addClass("btosaida").appendTo(divRetirada);
 
-    // Função para validar os campos do formulário de adição
-    function validarCliente() {
-        const nome = $("#adnome").val();
-        const telefone = $("#adtelefone").val();
-        const endereco = $("#adendereco").val();
-        const valor = $("#advalor").val();
-        const taxa = $("#adtaxa").val();
-        const parcelas = $("#adparcelas").val();
-
-        if (nome === '' || telefone === '' || endereco === '' || valor === '' || taxa === '' || parcelas === '') {
-            alert("Por favor, preencha todos os campos.");
-            return false;
+            $("#listaclientes").append(novoCliente);
+        }
+    
+        // Função para limpar os campos do formulário de adição após adicionar um cliente
+        function limparCampos() {
+            $("#adnome, #adtelefone, #adendereco, #comercio, #advalor, #adtaxa, #adparcelas").val("");
         }
 
-        return true;
-    }
+        $("#listaclientes").on("click", ".btosaida", function() {
+            const clienteNome = $(this).closest(".cliente").find(".nome").text();
 
-    // Função para adicionar um novo cliente à lista
-    function adicionarCliente() {
-        const cliente = {
-            nome: $("#adnome").val(),
-            telefone: $("#adtelefone").val(),
-            comercio: $("#comercio").val(),
-            endereco: $("#adendereco").val(),
-            valor: parseFloat($("#advalor").val()),
-            juros: parseFloat($("#adtaxa").val()), 
-            parcelas: parseInt($("#adparcelas").val())
-        };
-
-        const taxaDecimal = cliente.juros / 100;
-        const valorTotal = cliente.valor * (1 + taxaDecimal);
-        const valorParcela = valorTotal / cliente.parcelas;
-
-        // Criar novo elemento div para representar o cliente
-        const novoCliente = $("<div>").addClass("cliente");
-
-        
-        $("<button>").text("Editar cliente").addClass("editar").appendTo(novoCliente);
-
-        $("<h2>").text(cliente.nome).addClass("nome").appendTo(novoCliente);
-
-        $("<p>").text("Endereço:  " + cliente.endereco).addClass("endereco").appendTo(novoCliente);
-
-
-        $("<p>").text("Tipo de comércio:  " + cliente.comercio).addClass("tipocomercio").appendTo(novoCliente);
-
-
-        $("<p>").text("Telefone: " + cliente.telefone).addClass("telefone").appendTo(novoCliente);
+            alert(clienteNome);  
+        });
+    });
     
 
-        $("<p>").text("Valor Emprestado:  " + cliente.valor).addClass("valor").appendTo(novoCliente);
-       
-
-        $("<p>").text("Valor da Parcela:  " + (valorParcela.toFixed(2))).addClass("valorparcela").appendTo(novoCliente);
-
-        
-        $("<p>").text("Número de Parcelas:  " + cliente.parcelas).addClass("numeroparcelas").appendTo(novoCliente);
-
-
-        $("<p>").text("Taxa de Juros:  " + cliente.juros + "%").addClass("juros").appendTo(novoCliente);
-
-
-        $("<p>").text("Total a Pagar:  " + valorTotal.toFixed(2)).addClass("total").appendTo(novoCliente);
-
-
-        let botaoPagamento = $("<button>").text("Pagamento").attr("id", "pagamento");
-
-        botaoPagamento.appendTo(novoCliente);
-
-        $("#listaclientes").append(novoCliente);
-    }
-
-    // Função para limpar os campos do formulário de adição após adicionar um cliente
-    function limparCampos() {
-        $("#adnome, #adtelefone, #adendereco, #advalor, #adtaxa, #adparcelas").val("");
-    }
-
-    $("#listaclientes").on("click", "#pagamento", function() {
-        $("#retirada").show();
     
-        let clienteRetirando = $(this).closest(".cliente");
-
-        $("#textosaida").text("Digite o valor de parcelas pagas por " + clienteRetirando.find(".nome").text());
-
-        
-    });
-
-
-    $("#retirada").on("click", "#retirar", function() {
-        
-        alert("oi");
-    });
     
 
 
